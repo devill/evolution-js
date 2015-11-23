67662027dbd639af02fd193a8b7ae7882a3e07e7
devill <devillmeister@gmail.com>
2015-11-23 18:23:24 +0100

    Single pixel eys sight

--- creature.js
+++ creature.js
@@ -51,8 +51,8 @@ class Creature {
         let rx = this._position['x'] - position['x'];
         let ry = this._position['y'] - position['y'];
 
-        let nx = rx * Math.cos(direction) - ry * Math.sin(direction);
-        let ny = rx * Math.sin(direction) + ry * Math.cos(direction);
+        let nx = rx * Math.cos(-direction) - ry * Math.sin(-direction);
+        let ny = rx * Math.sin(-direction) + ry * Math.cos(-direction);
 
         return {distanceFromEye: nx, distanceFromLineOfSight: Math.abs(ny) };
     }
