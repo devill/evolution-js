b8e725e8f3211829c4dbfaef06a8722ae471ec00
Gabor Balla <cavalierrant@gmail.com>
2015-12-02 01:11:20 +0100

    use webpack instead of browserify

--- gulpfile.js
+++ gulpfile.js
@@ -3,22 +3,31 @@
 let gulp = require('gulp'),
     batch = require('gulp-batch'),
     watch = require('gulp-watch'),
-    exec = require('child_process').exec;
+    webpack = require('webpack');
 
+let webpackConfig = {
+  entry: { app: './js/main.js' },
+  module: {
+    loaders: [ { test: /\.js$/, exclude: /node_modules/, loader: 'babel' } ]
+  },
+  devtool: 'inline-source-map',
+  output: { filename: 'dist/evolution.js' }
+};
 
 gulp.task('build', function(cb) {
-  exec('node node_modules/browserify/bin/cmd.js js/main.js -t babelify -d -o dist/evolution.js', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
+  webpack(webpackConfig, function(err, stats) {
+    if(err) throw new gutil.PluginError("webpack", err);
+    cb();
   });
 });
 
 gulp.task('default', function() {
-  gulp.start('build');
   gulp.src('js/hsl2rgb.js')
     .pipe(gulp.dest('dist'));
+
+  gulp.start('build');
   watch('js/**/*', batch(function (events, done) {
     gulp.start('build', done);
   }));
 });
+
