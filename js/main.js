5a14696576a03afcc4142f1c0acdd8673ad27fec
devill <devillmeister@gmail.com>
2015-12-02 21:08:55 +0100

    Show fps

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ let World = require('./world');
 
 let world = new World(document.getElementById("main-canvas"));
 world.iteration();
+
+let lastIterationCount = 0;
+setInterval(() => {
+    let fps = world._iterationNumber - lastIterationCount;
+    document.getElementById('stats').innerHTML = `Reached ${world._iterationNumber} at ${fps} fps`;
+    lastIterationCount = world._iterationNumber;
+}, 1000);
