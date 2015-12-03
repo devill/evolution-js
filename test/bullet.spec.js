58ed981c544c44e90d1a8acb51020f7a4247719c
Gabor Balla <cavalierrant@gmail.com>
2015-12-03 22:26:19 +0100

    add an assert

--- test/bullet.spec.js
+++ test/bullet.spec.js
@@ -1,14 +1,17 @@
 'use strict';
 
+let assert  = require('assert');
+
 let Bullet = require('../js/bullet');
 
 describe('Bullet', function() {
   describe('#position()', function() {
     it('should return position from constructor', function() {
-      let dummyPosition = 123;
+      let expectedPosition = 123;
       let dummyDirection = 456;
-      var bullet = new Bullet(dummyPosition, dummyDirection);
-      bullet.position();
+      var bullet = new Bullet(expectedPosition, dummyDirection);
+      assert.equal(expectedPosition, bullet.position());
     });
   });
 });
+
