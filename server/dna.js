ac05d265edee52c8c9bb37b99b646afc57c9c45d
Gabor Balla <cavalierrant@gmail.com>
2015-12-20 13:04:40 +0100

    change postgres promise library to have connection pooling

--- server/dna.js
+++ server/dna.js
@@ -1,36 +1,39 @@
 "use strict";
 
-let pg = require('co-pg')(require('pg'));
+let pg = require('pg');
+let pgp = require('pg-promise')();
 
-let storedDna;
 let connectionString = process.env.DATABASE_URL || 'postgres://postgres_user:postgres_pw@localhost:9932/postgres_db';
+let db = pgp(connectionString);
 
 function* store() {
-  let client = new pg.Client(connectionString);
-  yield client.connectPromise();
-  let result = yield client.queryPromise('INSERT INTO dna (dna) VALUES ($1) RETURNING id', [this.request.body]);
-  client.end();
-
-  let id = result.rows[0].id;
-  storedDna = this.request.body;
-  this.redirect(`/dna/${id}/`);
+  yield db.one('INSERT INTO dna (dna) VALUES ($1) RETURNING id', [this.request.body])
+    .then((data) => {
+      this.redirect(`/dna/${data.id}/`);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 function* load(id) {
-  let client = new pg.Client(connectionString);
-  yield client.connectPromise();
-  let result = yield client.queryPromise('SELECT * FROM dna WHERE id = $1', [id]);
-  client.end();
-
-  this.body = JSON.parse(result.rows[0].dna);
+  yield db.one('SELECT * FROM dna WHERE id = $1', [id])
+    .then((data) => {
+      this.body = JSON.parse(data.dna);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 function* random() {
-  let client = new pg.Client(connectionString);
-  yield client.connectPromise();
-  let result = yield client.queryPromise('SELECT id, random() as r FROM dna ORDER BY r LIMIT 1');
-  let id = result.rows[0].id;
-  this.redirect(`/dna/${id}/`);
+  yield db.one('SELECT id, random() as r FROM dna ORDER BY r LIMIT 1')
+    .then((data) => {
+      this.redirect(`/dna/${data.id}/`);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
 
 module.exports = {
