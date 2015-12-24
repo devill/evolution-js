c4bae8f81394f30e720f8c347b1ee2213ec1bb64
devill <devillmeister@gmail.com>
2015-12-24 21:19:02 +0100

    Neat dna mixer class extract refactoring - moved evolve topology with new connection

--- client/js/neat_dna.js
+++ client/js/neat_dna.js
@@ -20,31 +20,20 @@ class NeatDna extends BaseDna {
     }
 
     mix(other_dna) {
-        if(getParameterByName('refactor')) {
-            let this_is_primary = Math.random() < 0.5;
-            let primary = (this_is_primary) ? this._dna : other_dna._dna;
-            let secondary = (!this_is_primary) ? this._dna : other_dna._dna;
+        let this_is_primary = Math.random() < 0.5;
+        let primary = (this_is_primary) ? this._dna : other_dna._dna;
+        let secondary = (!this_is_primary) ? this._dna : other_dna._dna;
 
-            console.log("Mixing:");
-            console.log(primary, secondary);
+        console.log("Mixing:");
+        console.log(primary, secondary);
 
-            let mixed = (new NeatDnaMixer(primary,secondary)).mix();
+        let mixed = (new NeatDnaMixer(primary,secondary)).mix();
 
-            console.log(mixed);
-            return new NeatDna({
-                id: uuid.v4(),
-                nodes: mixed.nodes,
-                connections: mixed.connections,
-                egg_color: this._mixEggColor(other_dna),
-                color: this._mixColor(other_dna),
-                eye_size: this._mixEyeSize(other_dna),
-                sight_resolution: this.sightResolution()
-            });
-        }
-
+        console.log(mixed);
         return new NeatDna({
             id: uuid.v4(),
-            connections: this.evolveConnections(other_dna),
+            nodes: mixed.nodes,
+            connections: mixed.connections,
             egg_color: this._mixEggColor(other_dna),
             color: this._mixColor(other_dna),
             eye_size: this._mixEyeSize(other_dna),
@@ -52,105 +41,8 @@ class NeatDna extends BaseDna {
         });
     }
 
-    evolveConnections(other_dna) {
-        return this.evolveTopology(this.mixConnections(other_dna));
-    }
-
-    mixConnections(other_dna) {
-        let this_is_primary = Math.random() < 0.5;
-        let primary = (this_is_primary) ? this._dna.connections : other_dna._dna.connections;
-        let secondary = (!this_is_primary) ? this._dna.connections : other_dna._dna.connections;
-
-        let innovation_hash = {};
-        secondary.forEach(connection => {
-            innovation_hash[connection.innovation] = connection;
-        });
-
-        return primary.map(connection => {
-            if(connection.enabled && innovation_hash[connection.innovation]) {
-                return {
-                    enabled:true,
-                    inNode: connection.inNode,
-                    outNode: connection.outNode,
-                    weight: Math.bimodalValueMix(connection.weight, innovation_hash[connection.innovation].weight) + chance.normal(),
-                    innovation: connection.innovation
-                };
-            } else {
-                return {
-                    enabled: connection.enabled,
-                    inNode: connection.inNode,
-                    outNode: connection.outNode,
-                    weight: connection.weight + chance.normal(),
-                    innovation: uuid.v4()
-                };
-            }
-        });
-    }
-
-    evolveTopology(connections) {
-        let p = Math.random();
-        if(p < Config.instance().get('node_addition_probability')) {
-            return this.evolveTopologyWithNewNode(connections)
-        } else if (p < Config.instance().get('node_addition_probability') + Config.instance().get('edge_addition_probability')) {
-            return this.evolveTopologyWithNewConnection(connections)
-        } else {
-            return connections;
-        }
-    }
-
-    evolveTopologyWithNewConnection(connections) {
-
-        let outNode = this.findOutCandidate(connections);
-        let inNode = this.findInCandidate(connections, outNode);
-
-        if(this.connected(outNode,inNode, this.connectionsAsHash(connections))) { return connections; }
-        if(connections.filter(c => { return c.inNode == inNode && c.outNode == outNode; }).length > 0) { return connections; }
-
-        connections.push({
-            enabled: true,
-            inNode: inNode,
-            outNode: outNode,
-            weight: chance.normal(),
-            innovation: uuid.v4()
-        });
-
-        return this.topologicalSortConnections(connections);
-    }
-
-    findOutCandidate(connections) {
-        let nodes = this.outNodes(connections).concat(this.hiddenNodes(connections));
-        return nodes[Math.floor(Math.random()*nodes.length)];
-    }
-
-    findInCandidate(connections) {
-        let nodes = this.inNodes(connections).concat(this.hiddenNodes(connections));
-        return nodes[Math.floor(Math.random()*nodes.length)];
-    }
-
-
     connections() {
         return this._dna.connections;
-    }
-
-    connectionsAsHash(connections) {
-        let hash = {};
-        connections.forEach(c => {
-            if(!hash[c.inNode]) { hash[c.inNode] = [] }
-            hash[c.inNode].push(c);
-        });
-        return hash;
-    }
-
-    connected(inIndex,outIndex,connectionsHash) {
-        if(inIndex == outIndex) { return true; }
-        if(!connectionsHash[inIndex]) { return false; }
-
-        for(let i = 0; i < connectionsHash[inIndex].length; ++i) {
-            if(this.connected(connectionsHash[inIndex][i].outNode, outIndex, connectionsHash)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     inNodes(connections) {
@@ -172,66 +64,6 @@ class NeatDna extends BaseDna {
         return connections.filter(c => {
             return !isInt(c.outNode);
         }).map(c => { return c.outNode; }).unique();
-    }
-
-    topologicalSortConnections(connections) {
-        let hash = this.connectionsAsHash(connections);
-        let inNodes = this.inNodes(connections);
-        let visitedNodes = {};
-        let sortedConnections = [];
-
-        function exploreNode(nodeId)
-        {
-            if(!hash[nodeId] || visitedNodes[nodeId]) { return; }
-            hash[nodeId].forEach(c => {
-                exploreNode(c.outNode);
-                sortedConnections.push(c);
-            });
-            visitedNodes[nodeId] = true;
-        }
-
-        inNodes.forEach(n => {
-            exploreNode(n);
-        });
-
-        return connections;
-    }
-
-    evolveTopologyWithNewNode(connections) {
-        let i = Math.floor(Math.random() * connections.length);
-        let new_node_index = this.findNextNodeIndex(connections);
-
-        if(!connections[i].enabled) { return connections; }
-
-        connections[i].enabled = false;
-
-        connections.push({
-            enabled: true,
-            inNode: connections[i].inNode,
-            outNode: new_node_index,
-            weight: 1,
-            innovation: uuid.v4()
-        });
-
-        connections.push({
-            enabled: true,
-            inNode: new_node_index,
-            outNode: connections[i].outNode,
-            weight: connections[i].weight,
-            innovation: uuid.v4()
-        });
-
-        return connections;
-    }
-
-    findNextNodeIndex(connections) {
-        let i = 0;
-        connections.forEach(c => {
-            if(isInt(c.inNode)) {
-                i = Math.max(c.inNode,i);
-            }
-        });
-        return i + 1;
     }
 
     static generateRandomDna() {
--- client/js/neat_dna_mixer.js
+++ client/js/neat_dna_mixer.js
@@ -47,8 +47,8 @@ class NeatDnaMixer {
         let p = Math.random();
         if(p < Config.instance().get('node_addition_probability')) {
             return this.evolveTopologyWithNewNode(network_dna);
-        //} else if (p < Config.instance().get('node_addition_probability') + Config.instance().get('edge_addition_probability')) {
-        //    return this.evolveTopologyWithNewConnection(network_dna);
+        } else if (p < Config.instance().get('node_addition_probability') + Config.instance().get('edge_addition_probability')) {
+            return this.evolveTopologyWithNewConnection(network_dna);
         } else {
             return network_dna;
         }
@@ -99,6 +99,85 @@ class NeatDnaMixer {
 
     cloneNodes(nodes) {
         return JSON.parse(JSON.stringify(nodes));
+    }
+
+    evolveTopologyWithNewConnection(network_dna) {
+        let connections = network_dna.connections;
+
+        let outNode = this.findOutCandidate(network_dna);
+        let inNode = this.findInCandidate(network_dna, outNode);
+
+        if(this.connected(outNode,inNode, this.connectionsAsHash(connections))) { return network_dna; }
+        if(connections.filter(c => { return c.inNode == inNode && c.outNode == outNode; }).length > 0) { return network_dna; }
+
+        connections.push({
+            enabled: true,
+            inNode: inNode,
+            outNode: outNode,
+            weight: chance.normal(),
+            innovation: uuid.v4()
+        });
+
+        return {
+            nodes: network_dna.nodes,
+            connections: this.topologicalSortConnections(network_dna)
+        };
+    }
+
+    findOutCandidate(network_dna) {
+        let nodes = network_dna.nodes['hidden'].concat(network_dna.nodes['out']);
+        return nodes[Math.floor(Math.random()*nodes.length)]['id'];
+    }
+
+    findInCandidate(network_dna) {
+        let nodes = network_dna.nodes['in'].concat(network_dna.nodes['hidden']);
+        return nodes[Math.floor(Math.random()*nodes.length)]['id'];
+    }
+
+
+    connected(inIndex,outIndex,connectionsHash) {
+        if(inIndex == outIndex) { return true; }
+        if(!connectionsHash[inIndex]) { return false; }
+
+        for(let i = 0; i < connectionsHash[inIndex].length; ++i) {
+            if(this.connected(connectionsHash[inIndex][i].outNode, outIndex, connectionsHash)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    connectionsAsHash(connections) {
+        let hash = {};
+        connections.forEach(c => {
+            if(!hash[c.inNode]) { hash[c.inNode] = [] }
+            hash[c.inNode].push(c);
+        });
+        return hash;
+    }
+
+    topologicalSortConnections(network_dna) {
+        let connections = network_dna.connections;
+        let hash = this.connectionsAsHash(connections);
+        let inNodes = network_dna.nodes['in'];
+        let visitedNodes = {};
+        let sortedConnections = [];
+
+        function exploreNode(nodeId)
+        {
+            if(!hash[nodeId] || visitedNodes[nodeId]) { return; }
+            hash[nodeId].forEach(c => {
+                exploreNode(c.outNode);
+                sortedConnections.push(c);
+            });
+            visitedNodes[nodeId] = true;
+        }
+
+        inNodes.forEach(n => {
+            exploreNode(n);
+        });
+
+        return connections;
     }
 
 }
